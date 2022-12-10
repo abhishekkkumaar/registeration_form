3b4a31159c0696f32eb1eae7c63f0eaf0f02c7ef
Abhishek Kumaar <39941327+abhishekkkumaar@users.noreply.github.com>
2022-12-10 15:20:57 +0530

    Add files via upload

---
+++ tryscript.js
@@ -0,0 +1,31 @@
+//let fname = ;
+console.log(document.getElementById('fname'));
+/* 
+let lname = document.getElementsByName('lname').value;
+console.log(lname);
+
+let add=document.getElementsByName('add').value;
+console.log(add);
+
+let pin=document.getElementsByName('pin').value;
+console.log(pin);
+
+let country=document.getElementsByName('#country').value;
+console.log(country);
+
+let gender=document.getElementsByName('input[name="gender"]:checked').value;
+console.log(gender);
+
+let food=[];
+let checkbox = document.getElementByName('Food').value;
+for (let i=0;i<checkbox;i++){
+    if(checkbox[i].checked){
+        food.push(checkbox[i].value);
+    }
+}
+console.log(food);
+
+  function myFunction() {
+  document.getElementById("form").reset();
+}
+ */
